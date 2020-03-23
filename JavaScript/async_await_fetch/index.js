a13ed33c3c380ae93945f618d2c58ffd60655efa
Oleksii Kovalchuk <oleksiij.ko@gmail.com>
2020-03-23 12:23:37 +0200

    async_await

---
+++ JavaScript/async_await_fetch/index.js
@@ -0,0 +1,32 @@
+const delay = ms => {
+  return new Promise(r => setTimeout(() => r(), ms));
+};
+
+const url = "https://jsonplaceholder.typicode.com/todos";
+
+// function fetchTodos() {
+//   console.log("Fetch todo started...");
+//   return delay(2000)
+//     .then(() => fetch(url))
+//     .then(response => response.json());
+// }
+
+// fetchTodos()
+//   .then(data => console.log("Data", data))
+//   .catch(err => console.error(err));
+
+async function fetchAsyncTodos() {
+  console.log("Fetch todo started...");
+  try {
+    await delay(2000);
+    const response = await fetch(url);
+    const data = await response.json();
+    console.log("Data", data);
+  } catch (err) {
+    console.error(err);
+  } finally {
+    console.log("Finally!");
+  }
+}
+
+fetchAsyncTodos();
--- JavaScript/class/index.js
+++ JavaScript/class/index.js
@@ -4,50 +4,50 @@
 // //   hasTail: true
 // // };
 
-// class Animal {
-//   static type = "ANIMAL";
+class Animal {
+  static type = "ANIMAL";
 
-//   constructor(options) {
-//     this.name = options.name;
-//     this.age = options.age;
-//     this.hasTail = options.hasTail;
-//   }
+  constructor(options) {
+    this.name = options.name;
+    this.age = options.age;
+    this.hasTail = options.hasTail;
+  }
 
-//   voice() {
-//     console.log("I am animal!");
-//   }
-// }
+  voice() {
+    console.log("I am animal!");
+  }
+}
 
-// // const animal = new Animal({
-// //   name: "Animal",
-// //   age: 5,
-// //   hasTail: true
-// // });
-
-// class Cat extends Animal {
-//   static type = "CAT";
-
-//   constructor(options) {
-//     super(options);
-//     this.color = options.color;
-//   }
-
-//   voice() {
-//     super.voice();
-//     console.log("I am cat!");
-//   }
-
-//   get ageInfo() {
-//     return this.age * 7;
-//   }
-// }
-
-// const cat = new Cat({
-//   name: "Cat",
-//   age: 7,
-//   hasTail: true,
-//   color: "black"
+// const animal = new Animal({
+//   name: "Animal",
+//   age: 5,
+//   hasTail: true
 // });
+
+class Cat extends Animal {
+  static type = "CAT";
+
+  constructor(options) {
+    super(options);
+    this.color = options.color;
+  }
+
+  voice() {
+    super.voice();
+    console.log("I am cat!");
+  }
+
+  get ageInfo() {
+    return this.age * 7;
+  }
+}
+
+const cat = new Cat({
+  name: "Cat",
+  age: 7,
+  hasTail: true,
+  color: "black"
+});
 
 class Component {
   constructor(selector) {
